9bd950fc36f8827ec0efb164dde2396bc6aea031
Jisu Jung <jsjung@holix.com>
2021-11-23 17:44:21 +0900

    Theming

--- week 1/Jetpack Compose basics/BasicsCodelab/app/src/main/java/com/example/basicscodelab/MainActivity.kt
+++ week 1/Jetpack Compose basics/BasicsCodelab/app/src/main/java/com/example/basicscodelab/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.example.basicscodelab
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
@@ -21,9 +23,11 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MessageCard(
-                msg = Message("Android", "Jetpack Compose")
-            )
+            BasicsCodelabTheme {
+                MessageCard(
+                    msg = Message("Android", "Jetpack Compose")
+                )
+            }
         }
     }
 }
@@ -41,25 +45,49 @@ fun MessageCard(msg: Message) {
             modifier = Modifier
                 .size(40.dp)
                 .clip(CircleShape)
+                .border(1.5.dp, MaterialTheme.colors.secondary, CircleShape)
         )
 
         Spacer(modifier = Modifier.width(8.dp))
 
         Column {
-            Text(text = msg.author)
+            Text(
+                text = msg.author,
+                color = MaterialTheme.colors.secondaryVariant,
+                style = MaterialTheme.typography.subtitle2
+            )
+
             Spacer(modifier = Modifier.height(4.dp))
-            Text(text = msg.body)
+
+            Surface(
+                shape = MaterialTheme.shapes.medium,
+                elevation = 1.dp
+            ) {
+                Text(
+                    text = msg.body,
+                    modifier = Modifier.padding(all = 4.dp),
+                    style = MaterialTheme.typography.subtitle2
+                )
+            }
         }
     }
 }
 
+@Preview(name = "LightMode")
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    showBackground = true,
+    name = "Dark Mode"
+)
 @Preview
 @Composable
 fun PreviewMessageCard() {
-    MessageCard(
-        msg = Message(
-            "Colleague",
-            "Hey, take a look at Jetpack Compose"
+    BasicsCodelabTheme {
+        MessageCard(
+            msg = Message(
+                "Colleague",
+                "Hey, take a look at Jetpack Compose"
+            )
         )
-    )
+    }
 }
